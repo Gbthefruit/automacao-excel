c55566022f6a3b3839e8d7654454ef4e73d61924
Gabriel de Souza <gabrielcunhagcs123@gmail.com>
2026-04-28 10:51:37 -0300

    Primeira versão do algoritmo de automação

---
+++ README.md
@@ -0,0 +1,9 @@
+# automacao-excel
+
+Automação de mensagens de boas vindas para números inclusos em uma tabela.
+
+O algoritmo procura o arquivo excel mencionado utilizando a biblioteca OpenPyXL, e vasculha cada linha, pegando o nome e o número das pessoas, e para cada linha envia uma mensagem que é buscada no arquivo de texto que contém a mensagem desejada.
+Para o controle de fluxo, ele retorna o nome e o número da pessoa no terminal durante a execução, e também o
+próprio PyWhatKit cria um arquivo de texto na pasta contendo o relatório com a data, hora, número e a mensagem que foi enviada.
+
+Ainda não está pronto, estou aprendendo a utilizar essas bibliotecas e conforme o tempo estarei desenvolvendo melhorias.
---
+++ testewpp.py
@@ -0,0 +1,52 @@
+import pywhatkit as wpp
+from openpyxl import load_workbook as xl
+
+erro = None
+
+try:
+    # abre o arquivo excel
+    arquivo_xl = xl("contatosWpp.xlsx")
+    aba_contatos = arquivo_xl["Contatos"]
+    ultima_linha = aba_contatos.max_row
+
+    lista_celulares = []
+    lista_nomes = []
+
+    # verifica os números e nomes da planilha
+    for linha in range(2, ultima_linha + 1):
+        cell = aba_contatos.cell(row=linha, column=1)
+        nome = aba_contatos.cell(row=linha, column=2).value
+
+        if not cell:       
+            break
+
+        cell_format = '+' + str(cell.value)
+        lista_celulares.append(cell_format)
+        lista_nomes.append(nome)
+
+except FileNotFoundError:
+    erro = FileExistsError
+    print("Arquivo Excel não encontrado")
+
+
+if erro is None:
+    try:
+        # abre o arquivo.txt para copiar a mensagem de saudação
+        with open("saudacao.txt", 'r', encoding="utf-8") as arq_saudacao:
+            mensagem = arq_saudacao.read()
+            n = 0
+
+        # envia instantaneamente a mensagem
+        for cell in lista_celulares:
+            print(lista_nomes[n], cell)
+            n += 1
+            
+            wpp.sendwhatmsg_instantly (
+                cell,
+                mensagem,
+                wait_time=15,
+                tab_close=True
+            )
+            
+    except FileNotFoundError:
+        print("Arquivo de texto não encontrado.")
